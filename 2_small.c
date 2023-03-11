4711a808a974657d95e1360a03a17f04b5f58d04
Jaydev Dwivedi <119089803+jaydevdwd@users.noreply.github.com>
2023-03-11 21:03:56 +0530

    Update 2_small.c

--- 2_small.c
+++ 2_small.c
@@ -11,7 +11,7 @@ int main()
         while (t--)
         {
             scanf("%f", &n);
-            float accent_color = 0, regular_color = 0, hour = 0;
+            float accent_color = 0, regular_color = 0, total_time = 0;
             for (int i = 0; i < n; i++)
             {
                 scanf("%f %f %f %f", &total, &r, &s, &h);
@@ -20,9 +20,9 @@ int main()
 
                 accent_color = accent_color + (float)(total_walls / 3) * (1.5);
                 regular_color = regular_color + (float)((2 * total_walls) / 3) * (2.25);
-                hour = hour + (total_walls / 3) * 2.5 + ((2 * total_walls) / 3) * 3.25;
+                total_time = total_time + (total_walls / 3) * 2.5 + ((2 * total_walls) / 3) * 3.25;
             }
-            printf("%0.2f,", hour);
+            printf("%0.2f,", total_time);
             printf("%0.2f,", accent_color);
             printf("%0.2f\n", regular_color);
         }
