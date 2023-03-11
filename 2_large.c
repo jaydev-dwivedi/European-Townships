c30ceb7009d93ff0cb166e3e6332b6fa7ed9b6fd
Jaydev Dwivedi <119089803+jaydevdwd@users.noreply.github.com>
2023-03-11 19:12:24 +0530

    Add files via upload

---
+++ 2_large.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("ET_large.txt", "r", stdin);
+    freopen("output_large.txt", "w", stdout);
+#endif
+        float t, n, total, r, s, h;
+        scanf("%f", &t);
+        while (t--)
+        {
+            scanf("%f", &n);
+            float accent_color = 0, regular_color = 0, hour = 0;
+            for (int i = 0; i < n; i++)
+            {
+                scanf("%f %f %f %f", &total, &r, &s, &h);
+
+                float total_walls = 6 * h + 4 * s + 3 * r;
+
+                accent_color = accent_color + (float)(total_walls / 3) * (1.5);
+                regular_color = regular_color + (float)((2 * total_walls) / 3) * (2.25);
+                hour = hour + (total_walls / 3) * 2.5 + ((2 * total_walls) / 3) * 3.25;
+            }
+            printf("%0.2f,", hour);
+            printf("%0.2f,", accent_color);
+            printf("%0.2f\n", regular_color);
+        }
+}
